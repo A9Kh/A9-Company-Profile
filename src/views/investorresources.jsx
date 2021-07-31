ac2bddaaec2e2561a14caf609f5547f7d5e2f818
Layhout <85871695+Layhout@users.noreply.github.com>
2021-07-31 14:55:02 +0700

    fix image investor page

--- src/views/investorresources.jsx
+++ src/views/investorresources.jsx
@@ -2,6 +2,7 @@ import Cover from 'components/Cover';
 import Corporate from 'components/investorresources/Corporate';
 import FAQs from 'components/investorresources/FAQs';
 import React from 'react';
+import investorImg from "assets/images/investorResourcse/investorImg.jpg"
 import { useTranslation } from 'react-i18next';
 
 const InvestorPage = () => {
@@ -11,7 +12,7 @@ const InvestorPage = () => {
     return (
         <main>
             <section>
-                <Cover img="http://www.sbsph.com/wp-content/uploads/2018/04/investor-relations.jpg" title={t("label_investor_overview")} />
+                <Cover img={investorImg} title={t("label_investor_overview")} />
             </section>
             <section className="container mx-auto max-w-7xl mb-10 lg:mb-20">
                 <Corporate />
